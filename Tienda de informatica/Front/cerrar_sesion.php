e948875dbb84e164d9e289755ae56c5962e77f20
GuilleMedina96 <guiilleeemedina@gmail.com>
2023-10-16 19:04:50 -0300

    corregido el cierre de sesión

--- Tienda de informatica/Front/cerrar_sesion.php
+++ Tienda de informatica/Front/cerrar_sesion.php
@@ -5,7 +5,7 @@ session_start();
 session_destroy();
 
 
-header("location : index.php"); // lo redirigimos al usuario al index donde no esta registrado ni ha iniciado sesion
+header("location: index.php"); // lo redirigimos al usuario al index donde no esta registrado ni ha iniciado sesion
 exit();
 
 ?>
