6dd0db475adc2e6a0d02de29e2fa459789c01424
franco lescano <franlesk@gmail.com>
2023-10-16 21:43:12 -0300

    Icono carrito y los estilos

--- Tienda de informatica/Front/navbar.php
+++ Tienda de informatica/Front/navbar.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de Informática</title>
-    <!-- Agrega tus enlaces a archivos CSS y otros elementos del encabezado aquí -->
 </head>
 <body>
     <header>
