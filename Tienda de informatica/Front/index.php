e9ef7a57565330d2e20a81ba37ebebbe8ef38892
briantabo <brian.taboada@hotmail.com>
2023-10-19 17:36:39 -0300

    Se reenombro home.php como index y borre el viejo

--- Tienda de informatica/Front/index.php
+++ Tienda de informatica/Front/index.php
@@ -1,29 +1,26 @@
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  href="./Estilos/navbar.css">
-    <title>Tienda de Informática</title>
-    
-    </head>
-    <body>
-    
-  <?php include 'navbar.php'; ?>
+    <title>Tienda de Informática - Inicio</title>
+    <link rel="stylesheet"  href="Estilos/navbar.css">
+    <link rel="stylesheet"  href="Estilos/home.css">
+</head>
+<body>
 
-        <main>
-            <!-- Contenido principal de la página -->
-           <h2><div><section class="destacados"  style="color: black;">Destacados</h2><a href="" rel="" id="view-more">Ver más</a></div>
-           <h2> <div><section class="componentes" style="color: black;">Componentes de PC</h2><a href="" rel="" id="view-more">Ver más</a></div>
-           <h2><div><section class="componentes" style="color: black;">Perifericos </h2><a href="" rel="" id="view-more">Ver más</a></div>
-           <h2> <div><section class="monitores" style="color: black;">Monitores</h2><a href="" rel="" id="view-more">Ver más</a></div>
-           <h2> <div><section class="impresoras" style="color: black;">Impresoras</h2><a href="" rel="" id="view-more">Ver más</a></div>
-        </main>
-        
-        <footer>
-            <p>&copy; 2023 Tienda de Informática</p>
-        </footer>
-    </body>
-    </html>
-    
+<header>
+<?php include 'navbar.php'; ?>
+</header>
+
+<h2>
+    Productos Destacados
+</h2>
+
+<div id="product-container">
+       <?php include '../Controladores/controller-home.php'; ?>
+</div>
+</body>
+</html>
--- Tienda de informatica/Front/navbar.php
+++ Tienda de informatica/Front/navbar.php
@@ -10,7 +10,7 @@
         <h1>Tienda de Informática</h1>
         <nav>
             <ul>
-                <li><a href="home.php">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <li><a href="categorias.php">Productos</a></li>
                 <li><a href="carrito.php">Carrito de Compras</a></li>
                 <?php
