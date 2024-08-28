7d4bc3bf55a7c7d487335311967be4055dc6f82c
GuilleMedina96 <guiilleeemedina@gmail.com>
2024-08-27 23:15:54 -0300

    Primer commit

--- Tienda de informatica/Controladores/controller-categorias.php
+++ Tienda de informatica/Controladores/controller-categorias.php
@@ -34,26 +34,23 @@ if ($num_rows > 0) {
         echo '<p>' . $fila['producto_nombre'] . '</p>';
         echo '<p>' . "Precio: $" . $fila['producto_precio'] . '</p>';
         echo '<p>' . "Stock: " . $fila['producto_stock'] . '</p>';
-        
+
         // Agregar al carrito
         echo '<form method="POST" action="../Front/carrito.php">';
         echo '<input type="hidden" name="producto_id" value="' . $fila['producto_id'] . '">';
         echo '<input type="hidden" name="producto_nombre" value="' . $fila['producto_nombre'] . '">';
         echo '<input type="hidden" name="producto_precio" value="' . $fila['producto_precio'] . '">';
         echo '<input type="hidden" name="producto_stock" value="' . $fila['producto_stock'] . '">';
-        
+
         // Agregamos campo type number de entrada para la cantidad
         echo '<label for="cantidad">Cantidad: </label>';
         echo '<input type="number" class="campo_cantidad" id="cantidad" name="cantidad" value="0" min="1" max="' . $fila['producto_stock'] . '">';
-        
+
         echo '<button class="button-carrito" type="submit" name="agregar_carrito">Añadir al carrito</button>';
         echo '</form>';
-        
+
         echo '</div>';
     }
-}
-else {
+} else {
     echo 'No se encontraron productos.';
 }
-
-?>
